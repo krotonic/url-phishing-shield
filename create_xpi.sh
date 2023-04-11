506097cf2a4c5ea8a1e1b398a68d40752b797036
John Krotonic <krotonu+182382173921874@gmail.com>
2023-04-11 07:34:46 +0200

    Added create_xpi.sh script

---
+++ create_xpi.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Set the name of the output XPI file
+output_xpi="url_phishing_shield.xpi"
+
+# Remove the existing XPI file, if it exists
+if [ -f "$output_xpi" ]; then
+  rm "$output_xpi"
+fi
+
+# Create a new XPI file by zipping the extension files
+zip -r "$output_xpi" background.js icon.png manifest.json LICENSE
+
+echo "XPI file created: $output_xpi"
+
